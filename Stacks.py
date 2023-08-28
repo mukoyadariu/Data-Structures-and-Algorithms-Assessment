28ffadaa4ab5648b33748fb8c11e81f81ef815e1
mukoyadariu <mukoya.darius@student.moringaschool.com>
2023-08-28 09:49:46 +0300

    completed assignment

---
+++ Stacks.py
@@ -0,0 +1,17 @@
+def is_balanced(expression):
+    stack = []
+    for char in expression:
+        if char in '([{':
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            if char == ')' and stack[-1] == '(':
+                stack.pop()
+            elif char == ']' and stack[-1] == '[':
+                stack.pop()
+            elif char == '}' and stack[-1] == '{':
+                stack.pop()
+            else:
+                return False
+    return not stack
